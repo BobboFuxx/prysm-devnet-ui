9fd885142eff144d4bd413986b4449f8d650d1ea
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2024-12-08 21:25:37 -0800

    Create dashboard.jsx

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-semibold">Dashboard</h1>
+      <p>Welcome to Prysm.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
